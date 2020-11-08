455e4236bad3265fc40968582668d86001ae60bb
negar_a <neazimi777@gmail.com>
2020-11-08 10:07:36 +0330

    add gun gameobj & shooter script

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    private int NumberOfBullets;
+    
+    void Start()
+    {
+         NumberOfBullets = 50;
+    }
+    void Update()
+    { 
+        print("this is update ");
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("space key was pressed");
+            NumberOfBullets--;
+            print(NumberOfBullets);
+        }
+    }
+}
