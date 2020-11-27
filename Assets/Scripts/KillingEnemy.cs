657863964e24f116cf96e9d3c717c3ae544f1756
negar_a <neazimi777@gmail.com>
2020-11-27 15:45:08 +0330

    add killingEnemy

--- Assets/Scripts/KillingEnemy.cs
+++ Assets/Scripts/KillingEnemy.cs
@@ -2,28 +2,44 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 public class KillingEnemy : MonoBehaviour
 {
-    public GameObject bullet;
-    public Image frontGround;
-
-    void Start()
-    { 
-
-     } 
-
+  public GameObject bullet;
+   
+    public Image frontGround ;
+    bool m_enterBullrt = false;
 
     void Update()
     {
-        void OnTriggerEnter (Collider other)
-      {
+      
+        amountOfHealth(m_enterBullrt);
+    }
+
+void OnTriggerEnter (Collider other)
+{
+    print("OnTriggerEnter");
         if (other.gameObject == bullet)
          {
-            frontGround.fillAmount -= 0.33f ;
-           print (frontGround.fillAmount);
+           m_enterBullrt =true;
+           
          }
 
-       }
-    }
 }
-    
+  
+ void amountOfHealth(bool m_enterBullrt)
+ { 
+
+  
+  if ( m_enterBullrt && frontGround.fillAmount>=0)
+   {  
+    //  print(" update + m_enterBullrt");
+      frontGround.fillAmount -= 0.33f;
+      // Debug.Log( " frontGround.fillAmount; \t" + frontGround.fillAmount);
+
+   }
+
+}
+
+}
+      
